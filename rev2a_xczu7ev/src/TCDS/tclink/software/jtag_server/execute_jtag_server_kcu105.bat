885536838bb6dd43d9ae1f6d441420cb48cd957d
Alexander Madorsky <madorsky@uftrig01.dyndns.cern.ch>
2024-05-09 04:10:29 +0200

    removing defective project

--- rev2a_xczu7ev/src/TCDS/tclink/software/jtag_server/execute_jtag_server_kcu105.bat
+++
@@ -1,3 +0,0 @@
-call  C:\Xilinx\Vivado\2019.1\\.\\bin\\vivado.bat -mode batch -source jtag_server_kcu105.tcl
-
-pause
--- rev2a_xczu7ev/src/TCDS/tclink/software/jtag_server/execute_jtag_server_vcu118.bat
+++
@@ -1,3 +0,0 @@
-call  c:\Xilinx\Vivado\2019.1\\.\\bin\\vivado.bat -mode batch -source jtag_server_vcu118.tcl
-
-pause
